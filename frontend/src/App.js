96941bd1043cdbf6dea486ea6af980363f9708f9
Juraj <juraj@localhost.localdomain>
2020-01-26 21:44:00 +0100

    code cleanup; lodash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './App.css';
 import axios from 'axios';
-import dateFormat from 'dateformat';
 import {
     LineChart,
     CartesianGrid,
@@ -10,21 +9,29 @@ import {
     Tooltip,
     Legend,
     Line,
-    ComposedChart,
-    ReferenceLine,
     ResponsiveContainer,
-    AreaChart,
-    Area,
-    Bar
 } from "recharts";
 import moment from 'moment'
-
+import { sortBy } from 'lodash';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 // import PropTypes from 'prop-types';
 // https://github.com/Hacker0x01/react-datepicker
 
+const styles = {
+    chart: {
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+    },
+    yAxis: {
+        opacity: 0.7,
+        fontSize: '1.4rem',
+        fontWeight: '400',
+    }
+}
 
 export class App extends Component {
 
@@ -33,8 +40,6 @@ export class App extends Component {
         startDate: new Date(),
         endDate: new Date(),
         special: []
-
-
     }
 
     componentDidMount() {
@@ -42,43 +47,60 @@ export class App extends Component {
         this.fetchDaysData();
     }
 
-    onChangeStartDate = date => {
-        console.log("Start Date caming from datepicker: ", date)
-        this.setState({
-            startDate: date
-        }, () => this.fetchRangeData());
-        // console.log("Start Date after", this.state.startDate)
+    onChangeStartDate = startDate => {
+        console.log("Start Date caming from datepicker: ", startDate);
+        this.setState({ startDate }, this.fetchRangeData);
     };
-    onChangeEndDate = date => { // console.log("End Date before", this.state.endDate);
-        console.log("End Date caming from datepicker: ", date)
-        this.setState({
-            endDate: date
-        }, () => this.fetchRangeData());
-        // console.log("End Date after", this.state.endDate);
+
+    onChangeEndDate = endDate => {
+        console.log("End Date caming from datepicker: ", endDate);
+        this.setState({ endDate }, this.fetchRangeData);
     };
 
     fetchAllData = () => {
-        axios.get('http://backend/api/v1/data').then(res => this.setState({data: res.data}))
+        axios.get('http://backend/api/v1/data')
+            .then(({ data }) => data)
+            .then(data => sortBy(data, ['created']))
+            .then(data => this.setState({ data }));
+    };
+
+    preProcess = data => {
+        // parse dates and sort them by time
+        // 'forEach' allows modification inplace 
+        data.forEach(item => {
+            item.data = item.data.map(d => ({ ...d, created: +Date.parse(`2020 ${d.created}`) }));
+            item.data = sortBy(item.data, ['created']);
+        })
+
+        // sort final arrays by length
+        return sortBy(data, item => item.data.length);
+
     };
 
     fetchDaysData = () => {
-        axios.get('http://backend/api/v1/data/days').then(res => this.setState({special: res.data}))
+        axios.get('http://backend/api/v1/data/days')
+            // un-pack data property
+            .then(({ data }) => data)
+            // pre-process it (sort by length, then by time)
+            .then(data => this.preProcess(data))
+            // rename them to 'special' and store in state
+            .then(special => this.setState({ special }))
     };
 
-
-    fetchRangeData = () => { // console.log(dateFormat(this.state.startDate, "yyyy-MM-dd'T'HH:mm:ss"))
+    fetchRangeData = () => {
         console.log(this.state.data);
         console.log("Start date: ", this.state.startDate);
         console.log("End date: ", this.state.endDate);
         axios.post(`http://backend/api/v1/data/select`, null, {
             params: {
-                start: dateFormat(this.state.startDate, "yyyy-mm-dd'T'HH:MM:ss"),
-                end: dateFormat(this.state.endDate, "yyyy-mm-dd'T'HH:MM:ss")
+                start: moment(this.state.startDate).format("yyyy-mm-dd'T'HH:MM:ss"),
+                end: moment(this.state.endDate).format("yyyy-mm-dd'T'HH:MM:ss")
             }
-        }).then(res => this.setState({data: res.data}))
+        }).then(({ data }) => this.setState({ data }))
 
     };
 
+    getColor = () => '#' + (Math.random().toString(16) + "000000").substring(2, 8);
 
     render() { // console.log(this.state.temp)
         return (
@@ -89,117 +111,68 @@ export class App extends Component {
 
                 <ResponsiveContainer minWidth={400}
                     minHeight={350}>
-                    <LineChart data={
-                            this.state.data                            // sort by time
-                            .sort((a, b) => a.created > b.created ? 1 : -1)
-                        }
-                        margin={
-                            {
-                                top: 5,
-                                right: 30,
-                                left: 20,
-                                bottom: 5
-                            }
-                    }>
-
-                        <XAxis dataKey="created"/>
+                    <LineChart
+                        data={sortBy(this.state.data, ['created'])}
+                        margin={styles.chart}
+                    >
+                        <XAxis dataKey="created" />
                         <YAxis type="number"
-                            domain={
-                                [-40, 40]
-                            }/>
-                        <CartesianGrid strokeDasharray="1 1"/>
-                        <Tooltip/>
-                        <Legend/>
-                        <Line dataKey="temperature" stroke="#8884d8" type="monotone"/>
-                        <Line dataKey="humidity" stroke="#82ca9d" type="monotone"/>
+                            unit="°C"
+                            domain={[-40, 40]}
+                            style={styles.yAxis} />
+                        <CartesianGrid strokeDasharray="1 1" />
+                        <Tooltip />
+                        <Legend />
+                        <Line dataKey="temperature" stroke="#8884d8" type="monotone" />
+                        <Line dataKey="humidity" stroke="#82ca9d" type="monotone" />
                     </LineChart>
                 </ResponsiveContainer>
 
-                <DatePicker selected={
-                        this.state.startDate
-                    }
+                <DatePicker
                     showTimeSelect
-                    onChange={
-                        event => this.onChangeStartDate(event)
-                    }
-
-
+                    selected={this.state.startDate}
+                    onChange={this.onChangeStartDate}
                     timeFormat="HH:mm"
                     timeIntervals={15}
                     timeCaption="time"
-                    dateFormat="yyyy MMMM d, HH:mm"/>
+                    dateFormat="yyyy MMMM d, HH:mm" />
 
-
-                <DatePicker selected={
-                        this.state.endDate
-                    }
+                <DatePicker
                     showTimeSelect
-                    onChange={
-                        event => this.onChangeEndDate(event)
-                    }
+                    selected={this.state.endDate}
+                    onChange={this.onChangeEndDate}
                     timeFormat="HH:mm"
                     timeIntervals={15}
                     timeCaption="time"
-                    dateFormat="yyyy MMMM d, HH:mm"/>
+                    dateFormat="yyyy MMMM d, HH:mm" />
 
-
-                <ResponsiveContainer minWidth={400}
+                <ResponsiveContainer
+                    minWidth={400}
                     minHeight={350}>
-                    <LineChart 
-                    margin={
-                        {
-                            top: 5,
-                            right: 30,
-                            left: 20,
-                            bottom: 5
-                        }
-                }
-                    >
-                        <CartesianGrid strokeDasharray="1 1"/>
-                        <XAxis dataKey="created"
-                            // type="time"
-                            allowDuplicatedCategory={false} 
-                            connectNulls={false} 
-                            tickFormatter = {(unixTime) => {
-                                console.log(unixTime)
-                                return moment(unixTime).format('LTS')
-                            }}/>
-                        <YAxis dataKey="temperature" type="number"
-                            domain={
-                                [-40, 40]
-                            }/>
-                        <Tooltip/>
-                        <Legend/> {
-                        this.state.special
-                            // sort by length
-                            .sort((a, b) => a.length > b.length ? 1 : -1)
-                            .map(s => (
-                            <Line dataKey="temperature"
-                                data={
-                                    s.data
-                                        .map(d => ({ ...d, created: +Date.parse(`2020 ${d.created}`)}))
-                                        // sort by time
-                                        .sort((a, b) => a.created > b.created ? 1 : -1)
-                                }
-                                name={
-                                    s.name
-                                }
-                                stroke={
-                                    {
-                                        '0': 'red',
-                                        '1': 'blue',
-                                        '2': 'green',
-                                        '3': 'yellow',
-                                        '4': 'orange',
-                                        '5': 'black',
-                                        '6': 'cyan'
-                                    }[s.name.substring('4')] || 'red'
-                                }
-                                key={
-                                    s.name
-                                }/>
-                        ))
-                    } </LineChart>
+                    <LineChart margin={styles.chart}>
+                        <CartesianGrid strokeDasharray="1 1" />
+                        <XAxis
+                            dataKey="created"
+                            allowDuplicatedCategory={false}
+                            connectNulls={false}
+                            tickFormatter={(unixTime) => moment(unixTime).format('LTS')} />
+                        <YAxis
+                            dataKey="temperature"
+                            type="number"
+                            style={styles.yAxis}
+                            unit="°C"
+                            domain={[-40, 40]} />
+                        <Tooltip />
+                        <Legend />
+                        {this.state.special.map(({ name, data }) => (
+                            <Line
+                                dataKey="temperature"
+                                key={name}
+                                data={data}
+                                name={name}
+                                stroke={this.getColor(name)} />
+                        ))}
+                    </LineChart>
                 </ResponsiveContainer>
             </div>
         )
